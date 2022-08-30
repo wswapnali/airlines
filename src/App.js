809e160e21adca45dddf0c226bdf14e7ee24d688
Swapnali Waghunde <swapnaliwaghunde@gmail.com>
2022-08-30 14:28:03 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import Airlines from './components/airlines/Airlines'
 
 function App() {
+
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Airlines 
+        /> 
     </div>
   );
 }
